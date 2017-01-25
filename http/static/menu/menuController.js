1da21849712bb155c8f0c218c2e7d13b6118d23d
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2017-01-25 23:11:08 +0100

    Inspiration setup webtools v3.0

--- http/static/menu/menuController.js
+++ http/static/menu/menuController.js
@@ -1,7 +1,3 @@
 ﻿angular.module('webtools').controller('menuController', ['$scope', 'menuModel', 'menuService', function ($scope, menuModel, menuService) {
     $scope.menuModel = menuModel;
-
-    $scope.toggleMenu = function () {
-
-    }
 }]);
