0ee15757341e7761a8e4897f086445ab34f7986d
Tommy Mikkelsen <tm@orstrand.dk>
2017-01-26 11:31:06 +0100

    Getting closer here. Seems like file isn't matched against valid ext. So yes...More logging needed to digest #227 :-(

--- Contents/Code/findMedia.py
+++ Contents/Code/findMedia.py
@@ -312,7 +312,11 @@ class findMedia(object):
 								if bAbort:
 									Log.Info('Aborted in getFiles')
 									raise ValueError('Aborted')
+								if DEBUGMODE:
+									Log.Debug('File extention is : ' + os.path.splitext(file)[1].lower())
 								if os.path.splitext(file)[1].lower() in Dict['findMedia']['VALID_EXTENSIONS']:
+									if DEBUGMODE:
+										Log.Debug('File has valid extention, so checking it out')
 									# File has a valid extention
 									if file.startswith('.') and Dict['findMedia']['IGNORE_HIDDEN']:
 										if DEBUGMODE:
