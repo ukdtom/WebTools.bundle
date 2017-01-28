ff46941f5b416e3ed79e1e8d525e4d6f810fbbc2
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2017-01-28 17:49:20 +0100

    Missing file added

---
+++ http/static/_shared/webtoolsController.js
@@ -0,0 +1,4 @@
+﻿angular.module('webtools').controller('webtoolsController', ['$scope', 'menuModel', 'themeModel', function ($scope, menuModel, themeModel) {
+    $scope.webtoolsThemeModel = themeModel;
+    $scope.webtoolsMenuModel = menuModel;
+}]);
