bba7140d3943a0ea2a94e3b859fd1ea8ab382f59
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2017-01-29 19:40:54 +0100

    Sub Man select/deselect all

--- http/static/sub_management/subController.js
+++ http/static/sub_management/subController.js
@@ -8,5 +8,19 @@
         if (!show.details) {
             subService.getShowDetails(show);
         }
+    };
+
+    $scope.getAllSubtitlesChecked = function (detail) {
+        for (var i = 0; i < detail.subtitles.length; i++) {
+            if (!detail.subtitles[i].checked) return false;
+        }
+        return true;
+    }
+    $scope.toggleAllSubtitlesChecked = function (detail) {
+        var allChecked = $scope.getAllSubtitlesChecked(detail);
+        for (var i = 0; i < detail.subtitles.length; i++) {
+            detail.subtitles[i].checked = !allChecked;
+        }
+        detail.subAllChecked = !allChecked;
     }
 }]);
