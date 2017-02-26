e2882bdc79cf90042dbfb95e7635391d33c015ab
Tommy Mikkelsen <tm@orstrand.dk>
2017-02-26 13:43:43 +0100

    Fixed #266

--- Contents/Code/gitV3.py
+++ Contents/Code/gitV3.py
@@ -1062,10 +1062,15 @@ class gitV3(object):
 	def getAtom_UpdateTime_Id(self, url, branch):		
 		# Build AtomUrl
 		atomUrl = url + '/commits/' + branch + '.atom'
-		# Get Atom
-		atom = HTML.ElementFromURL(atomUrl)
-		mostRecent = atom.xpath('//entry')[0].xpath('./updated')[0].text[:-6]
-		commitId = atom.xpath('//entry')[0].xpath('./id')[0].text.split('/')[-1][:10]
+		try:
+			# Get Atom
+			atom = HTML.ElementFromURL(atomUrl)
+			mostRecent = atom.xpath('//entry')[0].xpath('./updated')[0].text[:-6]
+			commitId = atom.xpath('//entry')[0].xpath('./id')[0].text.split('/')[-1][:10]
+		except Exception, e:
+			commitId = '0'
+			mostRecent = 'Not found'
+			pass
 		return {'commitId' : commitId, 'mostRecent' : mostRecent}
 
 	@classmethod
