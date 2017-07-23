5017f32aa9e34dfb71cd6993c313a95fc7b00016
ukdtom <tm@orstrand.dk>
2017-07-23 18:25:25 +0200

    Translation files

--- http/static/_shared/translations.js
+++ http/static/_shared/translations.js
@@ -1,5 +1,5 @@
-
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
-    gettextCatalog.setStrings('en', {"Home":"Home"});
-    gettextCatalog.setStrings('da', {"Home":"Hjem"});
+/* jshint -W100 */
+    gettextCatalog.setStrings('da', {"FindMedia":"Find Media","Home":"Hjem","Logs":"Logs","Sub Management":"Undertext managment","UAS":"UAS","Use the menu on the left side to navigate in WebTools.":"Brug menuen til venstre til at navigere med","Welcome to {[{webtoolsModel.versionFormated}]}":"Velkommen til {[{webtoolsModel.versionFormated}]}"});
+/* jshint +W100 */
 }]);
---
+++ translations/Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt){
+	grunt.loadNpmTasks('grunt-angular-gettext');
+	
+	grunt.initConfig({
+		nggettext_extract: {
+			pot: {
+				files: {
+					'../po/template.pot': ['../http/**/*.html']
+					}
+				},
+			},
+		nggettext_compile: {
+			all: {
+				files: {
+					'../http/static/_shared/translations.js': ['../po/*.po']
+					}
+				},
+			},
+		})
+};
