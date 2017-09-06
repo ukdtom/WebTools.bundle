9dcf0c0c272e60962683728a7de505d9a3d5ab8f
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2017-09-06 23:04:20 +0200

    #368 factory reset

--- http/static/factoryreset/frService.js
+++ http/static/factoryreset/frService.js
@@ -1,15 +1,26 @@
-﻿angular.module('webtools').service('frService', ['$http', 'webtoolsModel', 'webtoolsService', 'frModel', function ($http, webtoolsModel, webtoolsService, frModel) {
-    _this = this;
-
-    this.factoryReset = function (callback) { 
-        webtoolsModel.uasLoading = true;       
+﻿angular.module('webtools').service('frService', ['$http', 'webtoolsModel', 'webtoolsService', 'frModel', '$interval', '$window', function ($http, webtoolsModel, webtoolsService, frModel, $interval, $window) {
+    this.factoryReset = function (callback) {
+        webtoolsModel.globalLoading++;
         var url = webtoolsModel.apiV3Url + "/wt/reset";
         $http({
             method: "PUT",
-            url: url,            
+            url: url,
         }).then(function (resp) {
-                // Stuff here
-        }          
-        
-    )};                
+            if (callback) callback(resp.data);
+        }, function (errorResp) {
+            webtoolsModel.globalLoading--;
+            webtoolsService.log("frService.factoryReset - " + (errorResp.data ? errorResp.data : (errorResp ? errorResp : "NO ERROR MSG!")), "FactoryReset", true, url);
+        });
+
+        $interval(function () {
+            $http({
+                method: "GET",
+                url: "/",
+            }).then(function (resp) {
+                webtoolsModel.globalLoading--;
+                $window.location.reload(true);
+            }, function (errorResp) {
+            });
+        }, 5000);
+    };
 }]);
