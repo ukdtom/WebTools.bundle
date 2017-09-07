a0a34ca97d94057d1d7d734d8b0beed01ab98946
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2017-09-07 20:14:02 +0200

    Active path with be visible in menu with color

--- http/static/menu/menuController.js
+++ http/static/menu/menuController.js
@@ -1,12 +1,15 @@
-﻿angular.module('webtools').controller('menuController', ['$scope', 'menuModel', 'menuService', 'webtoolsModel', function ($scope, menuModel, menuService, webtoolsModel) {
+﻿angular.module('webtools').controller('menuController', ['$scope', 'menuModel', 'menuService', 'webtoolsModel', '$location', function ($scope, menuModel, menuService, webtoolsModel, $location) {
     $scope.webtoolsModel = webtoolsModel;
     $scope.menuModel = menuModel;
+
+    $scope.path = $location.path();
 
     $scope.init = function () {
     }
 
     $scope.navigateTo = function (path) {
         menuService.navigateTo(path);
+        $scope.path = $location.path();
     }
     $scope.redirectTo = function (url, isTargetBlank) {
         menuService.redirectTo(url, isTargetBlank);
