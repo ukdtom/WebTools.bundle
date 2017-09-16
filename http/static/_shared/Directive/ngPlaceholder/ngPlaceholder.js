805ecaf64e67e235f83403ed4a5966d0a8e05479
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2017-09-16 11:39:00 +0200

    #398

--- http/static/_shared/Directive/ngPlaceholder/ngPlaceholder.js
+++ http/static/_shared/Directive/ngPlaceholder/ngPlaceholder.js
@@ -4,6 +4,9 @@
         scope: { ngPlaceholder: "=" },
         link: function (scope, element, attr) {
             var placeholder = scope.ngPlaceholder;
+            scope.$watch("ngPlaceholder", function (newval) {
+                element.prop('placeholder', newval);
+            });
             element.prop('placeholder', placeholder);
         }
     };
