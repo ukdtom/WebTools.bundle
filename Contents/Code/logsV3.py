24ff0590beef0c0211a7aa99cb3b7b23d4037c41
kkatayama <katayama@udel.edu>
2022-02-23 21:09:11 -0500

    support for tv.plex and org.musicbrainz log files

--- Contents/Code/logsV3.py
+++ Contents/Code/logsV3.py
@@ -218,7 +218,7 @@ class logsV3(object):
             else:
                 try:
                     # if 'com.plexapp' in fileName:
-                    if next((ext for ext in {'tv.plex', 'org.musicbrainz', 'com.plexapp'} if ext in fileName), False):
+                    if [ext for ext in {'tv.plex', 'org.musicbrainz', 'com.plexapp'} if ext in fileName]:
                         file = os.path.join(
                             self.LOGDIR, 'PMS Plugin Logs', fileName)
                     else:
@@ -289,7 +289,7 @@ class logsV3(object):
                 return req
             fileName = String.Unquote(fileName, usePlus=False)
             # if 'com.plexapp' in fileName:
-            if next((ext for ext in {'tv.plex', 'org.musicbrainz', 'com.plexapp'} if ext in fileName), False):
+            if [ext for ext in {'tv.plex', 'org.musicbrainz', 'com.plexapp'} if ext in fileName]:
                 file = os.path.join(self.LOGDIR, 'PMS Plugin Logs', fileName)
             else:
                 file = os.path.join(self.LOGDIR, fileName)
